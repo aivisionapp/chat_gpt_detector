5bea2826a65f79907829af467513175ff852fabc
Abdullah Mohammed <a77x86@gmail.com>
2022-12-29 12:20:00 +0400

    add inital scripts

---
+++ scripts/check_modules.py
@@ -0,0 +1,10 @@
+
+import sys
+import pkgutil
+
+modules = sys.argv[1:]
+missing_modules = []
+for m in modules:
+    if pkgutil.find_loader(m) is None:
+        print('module', m, 'not found')
+        exit(1)
